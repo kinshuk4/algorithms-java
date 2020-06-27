6990ab484e0ac529239b80ecad8b22a4cb9dc4aa
Kinshuk <kinshuk.ram@gmail.com>
2020-06-27 11:39:58 +0200

    Make changes to variables in CoinChangeCount solution

--- src/main/java/com/vaani/dsa/algo/paradigm/dp/CoinChangeCount.java
+++ src/main/java/com/vaani/dsa/algo/paradigm/dp/CoinChangeCount.java
@@ -2,31 +2,31 @@ package com.vaani.dsa.algo.paradigm.dp;
 
 public class CoinChangeCount {
     // Returns the count of ways we can
-// sum S[0...m-1] coins to get sum n
-    static int countRecursive(int S[], int m, int sum) {
-        // If sum is 0 then there is 1 solution
+// amount coins[0...n-1] coins to get amount n
+    public static int countRecursive(int coins[], int n, int amount) {
+        // If amount is 0 then there is 1 solution
         // (do not include any coin)
-        if (sum == 0)
+        if (amount == 0)
             return 1;
 
         // If n is less than 0 then no
         // solution exists
-        if (sum < 0)
+        if (amount < 0)
             return 0;
 
-        // If there are no coins and sum
+        // If there are no coins and amount
         // is greater than 0, then no
         // solution exist
-        if (m <= 0 && sum >= 1)
+        if (n <= 0 && amount >= 1)
             return 0;
 
-        // Case 1. include current coin S[n] in solution and recur
-        // with remaining change (N - S[n]) with same number of coins
-        int include = countRecursive(S, m, sum - S[m]);
+        // Case 1. include current coin coins[n] in solution and recur
+        // with remaining change (N - coins[n]) with same number of coins
+        int include = countRecursive(coins, n, amount - coins[n]);
 
-        // Case 2. exclude current coin S[n] from solution and recur
+        // Case 2. exclude current coin coins[n] from solution and recur
         // for remaining coins (n - 1)
-        int exclude = countRecursive(S, m - 1, sum);
+        int exclude = countRecursive(coins, n - 1, amount);
 
         // return total ways by including or excluding current coin
         return include + exclude;
