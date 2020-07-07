d1c52a2600ef79440a8157525d4cb793f247d2a6
Kinshuk <kinshuk.ram@gmail.com>
2020-07-07 06:13:57 +0200

    Add invert binary tree

--- src/main/java/com/vaani/dsa/ds/algos/tree/binary/MaximumDepthOfBinaryTree.java
+++ src/main/java/com/vaani/dsa/ds/algos/tree/binary/MaximumDepthOfBinaryTree.java
@@ -2,7 +2,7 @@ package com.vaani.dsa.ds.algos.tree.binary;
 
 import com.vaani.dsa.ds.core.tree.binarytree.simple.BinaryTreeNode;
 
-/**
+/** https://leetcode.com/problems/maximum-depth-of-binary-tree/
  * Given a binary tree, find its maximum depth.
  * <p>
  * The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
